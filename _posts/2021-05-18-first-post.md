d904f0d3ae77cc773e8fdd9b84835c2817f8dc57
David Tran <tr.davidt@gmail.com>
2021-05-18 17:22:07 -0700

    Add blog page and first post (Jekyll)

---
+++ _posts/2021-05-18-first-post.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "First Blog Post"
+author: "David Tran"
+---
+
+Welcome! 
+
+You made it to our blog! 
---
+++ _site/script.js
@@ -0,0 +1,30 @@
+toggleNav = function() {  
+  navLinks = document.querySelectorAll(".nav-link");
+
+  for(i = 0; i < navLinks.length; i++) {
+    if(navLinks[i].style.display !== "block") {
+      navLinks[i].style.display = "block";
+      navLinks[i].classList.add("animate__animated", "animate__fadeIn");
+    }
+    else {
+      navLinks[i].style.display = "none";
+    }    
+  }  
+}
+
+toggleBtn = document.getElementById('toggle-btn');
+toggleBtn.addEventListener("click", toggleNav);
+
+function resizeNav() {
+    navHeight = document.querySelector(".nav-container").clientHeight;
+    navLinks = document.querySelectorAll(".nav-container .nav-link");
+
+  if (window.pageYOffset >= navHeight) {
+    document.querySelector("#nav-logo img").style.maxWidth = "10vw";
+  }
+  else {
+    document.querySelector("#nav-logo img").style.maxWidth = "15vw";
+  }
+}
+
+window.addEventListener("scroll", resizeNav);
